e931a4f1b3d295316e9f7c1da52a8411f647ed50
Stephen Vinuya <stephen.vinuya@auditmacs.com>
2026-04-01 22:10:56 +0800

    feat: updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
+    SharedModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [
     provideClientHydration(withEventReplay())
---
+++ src/app/public/public.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cap-public',
+  standalone: false,
+  templateUrl: './public.component.html',
+  styleUrl: './public.component.scss'
+})
+export class PublicComponent {
+
+}
